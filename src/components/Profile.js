8158465b3a7cb5c6a2a82d92c147c6207ff6e508
Gabriel Lima <devlimagabriel@gmail.com>
2020-04-19 18:33:21 -0300

    Setup axios and github api

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLongArrowAltRight } from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios';
 
 class Profile extends Component {
+    constructor(){
+        super();
+        this.state = {
+            profile: [],
+            error: false,
+            errorDetail: ''
+        }
+    }
+
+    componentDidMount() {
+        axios.get('https://ai.github.com/users/devlima')
+        .then(res => {
+            this.setState({
+                profile: res.data
+            });
+        })
+        .catch(error => {
+            this.setState({
+                error: true,
+                errorDetail: error
+            })
+        })
+    }
     render(){
+        if(this.state.error) {
+            return <h1 className="title h1">Try again later</h1>
+        }
         return (
             <div id="profile" className="profile">
-                <h1 className="title h1">Gabriel Lima</h1>
-                <p className="subtitle">Front-end Delevoper intern at Fattoria</p>
-                <nav>
+                <h1 className="title h1">{this.state.profile.name}</h1>
+                <p className="subtitle">{this.state.profile.bio}</p>
+                <p className="location">{this.state.profile.location}</p>
+                <nav className="profile-nav">
                     <ul>
                         <li><a href="https://www.linkedin.com/in/gbrl/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
                         <li><a href="https://github.com/devlima" target="_blank" rel="noopener noreferrer">GitHub</a></li>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,42 +1,50 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLongArrowAltLeft } from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios';
 
 class Projects extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            repos: [],
+            error: false,
+            errorDetail: ''
+        }
+    }
+
+    componentDidMount() {
+        axios.get('https://ai.github.com/users/devlima/repos')
+        .then(res => {
+            this.setState({
+                repos: res.data
+            });
+        })
+        .catch(error => {
+            this.setState({
+                error: true,
+                errorDetail: error
+            })
+        })
+    }
     render(){
+        if(this.state.error) {
+            return <h1 className="title h1">Try again later</h1>
+        }
         return (
             <div id="projects" className="projects">
                 <h2 className="title h2">Projects</h2>
                 <nav className="projects-nav">
                     <ul>
-                        <li>
-                            <a href="https://goopgle.com" target="_blank" rel="noopener noreferrer">music-recently</a>
-                            <p>Using spotify API</p>
-                        </li>
-                        <li>
-                            <a href="https://goopgle.com" target="_blank" rel="noopener noreferrer">music-recently</a>
-                            <p>Using spotify API</p>
-                        </li>
-                        <li>
-                            <a href="https://goopgle.com" target="_blank" rel="noopener noreferrer">music-recently</a>
-                            <p>Using spotify API</p>
-                        </li>
-                        <li>
-                            <a href="https://goopgle.com" target="_blank" rel="noopener noreferrer">music-recently</a>
-                            <p>Using spotify API</p>
-                        </li>
-                        <li>
-                            <a href="https://goopgle.com" target="_blank" rel="noopener noreferrer">music-recently</a>
-                            <p>Using spotify API</p>
-                        </li>
-                        <li>
-                            <a href="https://goopgle.com" target="_blank" rel="noopener noreferrer">music-recently</a>
-                            <p>Using spotify API</p>
-                        </li>
-                        <li>
-                            <a href="https://goopgle.com" target="_blank" rel="noopener noreferrer">music-recently</a>
-                            <p>Using spotify API</p>
-                        </li>
+                        {this.state.repos.map((repo, index) => {
+                            return (
+                                <li key={index}>
+                                    <a href={repo.homepage} target="_blank" rel="noopener noreferrer">{repo.name}</a>
+                                    <p>{repo.description}</p>
+                                </li>
+                            )
+                        })}
                     </ul>
                 </nav>
                 <a href="#profile" className="page-link page-link-left"> <FontAwesomeIcon icon={faLongArrowAltLeft} /> <span>Profile</span></a>
