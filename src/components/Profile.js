8809c2cfe26909553b1228cbc40cc0c39565769e
Gabriel Lima <devlimagabriel@gmail.com>
2020-04-21 22:11:25 -0300

    Refactor error message and fix cros error

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLongArrowAltRight } from '@fortawesome/free-solid-svg-icons';
 import axios from 'axios';
@@ -14,7 +14,7 @@ class Profile extends Component {
     }
 
     componentDidMount() {
-        axios.get('https://api.github.com/users/devlima')
+        axios.get('https://cors-anywhere.herokuapp.com/https://api.github.com/users/devlima')
         .then(res => {
             this.setState({
                 profile: res.data
@@ -28,25 +28,24 @@ class Profile extends Component {
         })
     }
     render(){
-        if(this.state.error) {
-            return <h1 className="title h1">Try again later</h1>
-        }
         return (
-            <div id="profile" className="profile">
-                <h1 className="title h1">{this.state.profile.name}</h1>
-                <p className="subtitle">{this.state.profile.bio}</p>
-                <p className="location">{this.state.profile.location}</p>
-                <nav className="profile-nav">
-                    <ul>
-                        <li><a href="https://www.linkedin.com/in/gbrl/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
-                        <li><a href="https://github.com/devlima" target="_blank" rel="noopener noreferrer">GitHub</a></li>
-                        <li><a href="http://t.me/limagabriel" target="_blank" rel="noopener noreferrer">Telegram</a></li>
-                        <li><a href="malito:devlimagabriel@gmail.com" target="_blank" rel="noopener noreferrer">devlimagabriel@gmail.com</a></li>
-                    </ul>
-                </nav>
-                <a href="#projects" className="page-link page-link-right"><span>Projects</span> <FontAwesomeIcon icon={faLongArrowAltRight} /></a>
-                
-            </div>
+            <Fragment>
+                <h2 className={this.state.error ? 'title h2' : 'error'}>Try again later</h2>
+                <div id="profile" className={this.state.error ? 'error' : 'profile'}>
+                    <h1 className="title h1">{this.state.profile.name}</h1>
+                    <p className="subtitle">{this.state.profile.bio}</p>
+                    <p className="location">{this.state.profile.location}</p>
+                    <nav className="profile-nav">
+                        <ul>
+                            <li><a href="https://www.linkedin.com/in/gbrl/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+                            <li><a href="https://github.com/devlima" target="_blank" rel="noopener noreferrer">GitHub</a></li>
+                            <li><a href="http://t.me/limagabriel" target="_blank" rel="noopener noreferrer">Telegram</a></li>
+                            <li><a href="malito:devlimagabriel@gmail.com" target="_blank" rel="noopener noreferrer">devlimagabriel@gmail.com</a></li>
+                        </ul>
+                    </nav>
+                    <a href="#projects" className="page-link page-link-right"><span>Projects</span> <FontAwesomeIcon icon={faLongArrowAltRight} /></a>    
+                </div>
+            </Fragment>
         )
     }
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,12 +10,17 @@ class Projects extends Component {
         this.state = {
             repos: [],
             error: false,
-            errorDetail: ''
+            errorDetail: '',
+            config: {
+                headers: { 'Access-Control-Allow-Origin': '*'}
+            }
         }
     }
 
     componentDidMount() {
-        axios.get('https://api.github.com/users/devlima/repos')
+        axios.get('https://cors-anywhere.herokuapp.com/https://api.github.com/users/devlima/repos', {
+            crossdomain: true
+        })
         .then(res => {
             this.setState({
                 repos: res.data
@@ -29,27 +34,24 @@ class Projects extends Component {
         })
     }
     render(){
-        if(this.state.error) {
-            return <h1 className="title h1">Try again later</h1>
-        }
         return (
-            <div id="projects" className="projects">
-                <h2 className="title h2">Projects</h2>
-                <nav className="projects-nav">
-                    <ul>
-                        {this.state.repos.map((repo, index) => {
-                            return (
-                                <li key={index}>
-                                    <a className="projects--link" href={repo.homepage} target="_blank" rel="noopener noreferrer">{repo.name}</a>
-                                    <p className="projects--description">{repo.description}</p>
-                                    <a className="projects--link-code" href={repo.html_url} target="_blank" rel="noopener noreferrer">code on github</a>
-                                </li>
-                            )
-                        })}
-                    </ul>
-                </nav>
-                <a href="#profile" className="page-link page-link-left"> <FontAwesomeIcon icon={faLongArrowAltLeft} /> <span>Profile</span></a>
-            </div>
+                <div id="projects" className={this.state.error ? 'error' : 'projects'}>
+                    <h2 className="title h2">Projects</h2>
+                    <nav className="projects-nav">
+                        <ul>
+                            {this.state.repos.map((repo, index) => {
+                                return (
+                                    <li key={index}>
+                                        <a className="projects--link" href={repo.homepage} target="_blank" rel="noopener noreferrer">{repo.name}</a>
+                                        <p className="projects--description">{repo.description}</p>
+                                        <a className="projects--link-code" href={repo.html_url} target="_blank" rel="noopener noreferrer">code on github</a>
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                    </nav>
+                    <a href="#profile" className="page-link page-link-left"> <FontAwesomeIcon icon={faLongArrowAltLeft} /> <span>Profile</span></a>
+                </div>
         )
     }
 }
